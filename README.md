3a16b50695bcb6faa81f592d2c8d49934d9818ed
Praagya288 <praagyajain2828@gmail.com>
2024-11-13 23:40:49 +0530

    made some changes

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-# React + Vite
-
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+continue from here
+https://youtu.be/TTx7Y3a7EmA?t=1334
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -4,13 +4,15 @@ const Login = () => {
   return (
     <div className='flex h-screen w-screen items-center justify-center' >
         <div className='border-2 rounded-xl border-emerald-600 p-20 ' >
-            <form className='flex flex-col items-center justify-center ' >
-                <div className='bg-white' >
-                    <h1 className='underline' >Hello</h1>
-                </div>
+            <form
+            
+            className='flex flex-col items-center justify-center ' >
+
             
                 <input
-                type="email" placeholder = "Enter email" 
+                type="email"
+
+                placeholder = "Enter email" 
                 required
                 className='outline-none bg-transparent border-2 border-emerald-600  py-3 text-xl px-5 rounded-full placeholder:text-gray-400 '  />
                 <input 
