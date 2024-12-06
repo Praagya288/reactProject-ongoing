70edb8382f3677ade3dbd8bc1915da846e263e41
Praagya288 <praagyajain2828@gmail.com>
2024-12-06 20:09:42 +0530

    project continued

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 continue from here
-https://youtu.be/TTx7Y3a7EmA?t=1334
+https://youtu.be/TTx7Y3a7EmA?t=2899
+
+
+
+
+
+
+Features to add later
+- star option to highlight the important task
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const[email , setEmail] = useState("") ;
+  const[password , setPassword] = useState("") ;
+
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+    console.log("email is " , email)
+    console.log("passowrd is " , password)
+
+    setEmail("")
+    setPassword("")
+  }
+
   return (
     <div className='flex h-screen w-screen items-center justify-center' >
         <div className='border-2 rounded-xl border-emerald-600 p-20 ' >
             <form
-            
+
+            onSubmit={(e)=>{
+              submitHandler(e) 
+            }}
+           
             className='flex flex-col items-center justify-center ' >
 
             
                 <input
+                value={email}
+                onChange={(e)=>{
+                  setEmail(e.target.value)
+                }}
                 type="email"
-
                 placeholder = "Enter email" 
                 required
                 className='outline-none bg-transparent border-2 border-emerald-600  py-3 text-xl px-5 rounded-full placeholder:text-gray-400 '  />
                 <input 
+                value={password}
+                onChange={(e)=>{
+                  setPassword(e.target.value)
+                }}
                 required
                 type="password" placeholder="Enter password"
                 className=' outline-none bg-transparent border-2 border-emerald-600  py-3 text-xl px-5 rounded-full  placeholder:text-gray-400 mt-3 '  />
